dc69631be4297abc509f6fd3d1a7576d45d4a36a
Cassandra <semisentientfungus@gmail.com>
2023-07-17 17:00:53 +0200

    retool library container population

--- script.js
+++ script.js
@@ -11,30 +11,52 @@ function addBook(object){
     myLibrary.push(object)
 }
 
-const table = document.getElementById('myTable')
+function populate(array){
+    for (element of array){
+        const div = document.createElement('div')
+        div.classList.add('book-element')
 
-function makeTable(array){
-    array.forEach(object => {
-        populate(object)
-    });
+        const title = document.createElement('div')
+        title.classList.add('book-title')
+        title.innerHTML = element.title
+        div.append(title)
+
+        const author = document.createElement('div')
+        author.classList.add('book-author')
+        author.innerHTML = element.author
+        div.append(author)
+
+        const pages = document.createElement('div')
+        pages.classList.add('book-pages')
+        pages.  innerHTML = element.pages
+        div.append(pages)
+
+        const read = document.createElement('button')
+        if (element.read == true){
+            read.classList.add('book-read')
+            read.innerHTML = "Read"
+        }
+        else{
+            read.classList.add('book-unread')
+            read.innerHTML = "Unread"
+        }
+        
+        div.append(read)
+
+        //add book to grid
+        grid.append(div)
+    }
 }
 
-function populate(object){
-    var row = table.insertRow()
-    var cell1 = row.insertCell(0)
-    var cell2 = row.insertCell(1)
-    var cell3 = row.insertCell(2)
-    var cell4 = row.insertCell(3)
-
-    cell1.innerHTML = object.title
-    cell2.innerHTML = object.author
-    cell3.innerHTML = object.pages
-    cell4.innerHTML = object.read
-}
 const book1 = new Book('Lord of the Rings', 'J RR Tolkien', 2600, false)
 const book2 = new Book('Wheel of Time', 'Robert Jordan', 2500, true)
+const book3 = new Book('Guards! Guards!', 'Sir Terry Pratchett', 600, true)
+
 
 addBook(book1)
 addBook(book2)
+addBook(book3)
 
-makeTable(myLibrary)
+const grid = document.querySelector('.library-grid')
+
+populate(myLibrary)
