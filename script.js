fd696c3ac4dd83f20a8d8b52e84c556b877d64ac
Cassandra <semisentientfungus@gmail.com>
2023-07-25 16:46:38 +0200

    finish form functionality

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 myLibrary = []
 let i = 0
+
 function Book(title, author, pages, read = false) {
     this.title = title;
     this.author = author;
@@ -7,10 +8,11 @@ function Book(title, author, pages, read = false) {
     this.read = read;
 }
 
+let book = new Book('title', 'author', 'pages', false)
+
 function addBook(object){
     myLibrary.push(object)
 }
-const book = new Book('title', 'author', 'pages', false)
 
 function appendDiv(object){
     const grid = document.querySelector('.library-grid')
@@ -45,67 +47,12 @@ function appendDiv(object){
         read.value = i
     }
     read.addEventListener('click', () => {
-        read_book(i)
+        read_book(read.value)
     })
     div.append(read)
     i++
     grid.append(div)
 }
-
-/*
-function populate(array){
-    let i = 0
-    for (element of array){
-        const div = document.createElement('div')
-        div.classList.add('book-element')
-
-        const title = document.createElement('div')
-        title.classList.add('book-title')
-        title.innerHTML = element.title
-        div.append(title)
-
-        const author = document.createElement('div')
-        author.classList.add('book-author')
-        author.innerHTML = element.author
-        div.append(author)
-
-        const pages = document.createElement('div')
-        pages.classList.add('book-pages')
-        pages.  innerHTML = element.pages
-        div.append(pages)
-
-        const read = document.createElement('button')
-        if (element.read == true){
-            read.classList.add('book-read')
-            read.innerHTML = "Read"
-            read.value = i
-        }
-        else{
-            read.classList.add('book-unread')
-            read.innerHTML = "Unread"
-            read.value = i
-        }
-        
-        div.append(read)
-        i++
-
-        //add book to grid
-        grid.append(div)
-        read.addEventListener('click',() => read_book(read.value))
-    }
-}
-
-function libraryClear(){
-    while(grid.firstChild){
-        grid.firstChild.remove()
-    }
-}
-
-function refreshGrid(){
-    libraryClear()
-    populate(myLibrary)
-}
-*/
 
 function read_book(index){
     const read = document.querySelector('.book-unread')
@@ -119,21 +66,6 @@ function read_book(index){
         read.classList.remove('book-unread')
     }
 }
-/*
-const book1 = new Book('Lord of the Rings', 'J RR Tolkien', 2600, false)
-const book2 = new Book('Wheel of Time', 'Robert Jordan', 2500, true)
-const book3 = new Book('Guards! Guards!', 'Sir Terry Pratchett', 600, true)
-
-
-const grid = document.querySelector('.library-grid')
-
-const btn = document.querySelector('.book-add')
-
-btn.addEventListener('click', () => refreshGrid())
-
-populate(myLibrary)
-
-*/
 
 const form = document.querySelector('.myform')
 
@@ -145,11 +77,19 @@ addBtn.addEventListener('click', () => {
 
 const formBtn = document.querySelector('.formbutton')
 
-formBtn.addEventListener('click', () => {
-    const title = document.getElementById('title')
-    const author = document.getElementById('author')
-    const pages = document.getElementById('pages')
-    const read = /*document.getElementById('readstatus')*/ true
-
-    book = {title, author, pages, read}
+formBtn.addEventListener('click', () => {   
+    const bookTitle = document.getElementById('title').value;
+    const bookAuthor = document.getElementById('author').value;
+    const bookPages = document.getElementById('pages').value;
+    const bookRead = document.getElementById('readstatus').checked;
+     
+    book = {
+        title: bookTitle,
+        author: bookAuthor,
+        pages: bookPages,
+        read: bookRead
+    }
+    appendDiv(book)
+    addBook(book)
+    form.style.display = "none"
 }); 
